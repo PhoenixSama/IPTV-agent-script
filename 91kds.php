e7b227a8c0244883f7010c430d3328c7ac55c90d
PhoenixSama <38131818+PhoenixSama@users.noreply.github.com>
2021-05-25 00:07:51 +0800

    Update 91kds.php

--- 91kds.php
+++ 91kds.php
@@ -1,7 +1,7 @@
 <?php
 $id=$_GET["id"];
 $tid=$_GET["tid"];
-$info=file_get_contents("http://wx.91kds.cn/key.php");
+$info=file_get_contents("http://m1.91kds.cn/fuck.php");
 preg_match('/"livekey":"(.*?)"/i',$info,$sn);
 $ts = "http://mlive.91kds.cn/b9/".$tid.".m3u8?id=".$id."&".$sn[1]."";
 header("Location:".$ts);
