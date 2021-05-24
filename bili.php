e604077b2848395d9c690d3e175ce2d3be636d43
PhoenixSama <38131818+PhoenixSama@users.noreply.github.com>
2021-05-25 00:08:22 +0800

    Create bili.php

---
+++ bili.php
@@ -0,0 +1,15 @@
+<?php
+$id=$_GET["id"];
+$url="https://api.live.bilibili.com/room/v1/Room/playUrl?platform=uwp_desktop&device=wp&expire=0&build=&mid=1&qn=4&npcybs=0&cid=".$id."&otype=json&appkey=&buvid=infoc&ts=&access_key=&sign=";
+     $ch = curl_init();
+     curl_setopt($ch, CURLOPT_URL, $url);
+     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+     curl_setopt($ch, CURLOPT_USERAGENT, "Bilibili UWP Client/2.13.7.0 (atelier39@outlook.com)" );
+     $data = curl_exec($ch);
+     curl_close($ch);
+preg_match('/"url":"(.*?)"/i',$data,$sn);
+$ts=str_replace('\u0026','&',$sn[1]);
+header("Location:".$ts);
+?>
